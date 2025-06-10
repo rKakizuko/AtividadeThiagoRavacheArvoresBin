9166b63baf8a8cb56f77b4f97ea429ec14596417
Rafael <rafaelshimomura@gmail.com>
2025-06-10 19:43:38 -0300

    Exercicio Parte 11 - Atualização da main para testar inserção e busca em ordem.

--- TodosOsExerciciosDasAulas/src/Main2.java
+++ TodosOsExerciciosDasAulas/src/Main2.java
@@ -1,46 +1,15 @@
 public class Main2 {
     public static void main(String[] args) {
-        ArvoreAVL arvoreAVL = new ArvoreAVL(); //copia da main passada atualizada
+        ArvoreAVL arvoreAVL = new ArvoreAVL();
 
-        System.out.println("Inserindo elementos:");
-        arvoreAVL.inserir("D");
-        arvoreAVL.inserir("B");
-        arvoreAVL.inserir("F");
-        arvoreAVL.inserir("A");
-        arvoreAVL.inserir("C");
-        arvoreAVL.inserir("E");
-        arvoreAVL.inserir("G");
-        arvoreAVL.inserir("H");
-        arvoreAVL.inserir("I");
-        arvoreAVL.inserir("J");
+        String [] chaves = {"10","20","30","40","50","25"};
 
+        for(String valor : chaves) {
+            arvoreAVL.raiz = arvoreAVL.inserirRecursivo(arvoreAVL.raiz, valor);
+        }
 
-        arvoreAVL.printTreeStructure();
-
-        System.out.println("\nTestando Contagens:");
-        arvoreAVL.contarNosSemRecursividade();
-        arvoreAVL.contarNosSemRecursividadePilha();
-        System.out.println("Contagem de nós (Recursivo): " + arvoreAVL.contarNos(arvoreAVL.raiz));
-        arvoreAVL.contarNosFolhaSemRecursividade();
-        System.out.println("Contagem de nós folha (Recursivo): " + arvoreAVL.contarNosFolha(arvoreAVL.raiz));
-
-        System.out.println("\nTestando Buscas:");
-        System.out.print("Pré-ordem (Recursivo): ");
-        arvoreAVL.buscaPreOrdem(arvoreAVL.raiz);
-        System.out.println();
-        arvoreAVL.buscaPreOrdemSemRecursividade();
-
-        System.out.print("Em-ordem (Recursivo): ");
+        System.out.println("Percurso em ordem da árvore AVL: ");
         arvoreAVL.buscaEmOrdem(arvoreAVL.raiz);
-        System.out.println();
-        arvoreAVL.buscaEmOrdemSemRecursividade();
-
-        System.out.print("Pós-ordem (Recursivo): ");
-        arvoreAVL.buscaPosOrdem(arvoreAVL.raiz);
-        System.out.println();
-        arvoreAVL.buscaPosOrdemSemRecursividade();
-
-        arvoreAVL.buscaEmNivel();
 
     }
 }
